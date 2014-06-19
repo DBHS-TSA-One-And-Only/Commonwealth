fe23b0dc1edcc4931e0d2c5d2b44a4821dc31abb
Forest Hu <foresthu2006@gmail.com>
2014-06-18 22:45:03 -0700

    fixed splitter class

--- src/commonwealth/sentencemanager/Splitter.java
+++ src/commonwealth/sentencemanager/Splitter.java
@@ -4,17 +4,39 @@ package commonwealth.sentencemanager;
 
 import java.util.ArrayList;
 
-
 public class Splitter {
-    
-    // this method splits the tokenized string sentence into clauses based on
-    // clause conjunction keywords or commas
-    // returns multiple arraylists in an arraylist of arraylists
-    // each arraylist in the returned arraylist will be a POTENTIAL clause
-    public static ArrayList<ArrayList<String>> splitClauses(ArrayList<String> tokenizedSentence){
-        
-        
-        return null;
-    }
-    
+
+	public static ArrayList<String> tokenizedSentence = new ArrayList<>(), charIdentifier = new ArrayList<>();
+	public static ArrayList<ArrayList<String>> clauses = new ArrayList<>(), charIdentifiers = new ArrayList<>();
+	
+
+	public Splitter(ArrayList<String> sentence, ArrayList<String> charIdentifier) {
+		tokenizedSentence = sentence;
+		this.charIdentifier= charIdentifier;
+	}
+
+	// this method splits the tokenized string sentence into clauses based on
+	// clause conjunction keywords or commas
+	// returns multiple arraylists in an arraylist of arraylists
+	// each arraylist in the returned arraylist will be a POTENTIAL clause
+	public static ArrayList<ArrayList<String>> splitClauses(
+			ArrayList<String> sentence) {
+		int previousIndex = 0;
+
+		for (int index = 0; index < tokenizedSentence.size(); index++) {
+			if (tokenizedSentence.get(index).matches("[,;]")) {
+				ArrayList<String> clauseHolder = new ArrayList<>(), identifierHolder = new ArrayList<>();
+				for (int i = previousIndex; i <= index; i++) {
+					clauseHolder.add(tokenizedSentence.get(i));
+					identifierHolder.add(charIdentifier.get(i));
+				}
+				clauses.add(clauseHolder);
+				charIdentifiers.add(identifierHolder);
+				previousIndex = index+1;
+			}
+		}
+		return clauses;
+	}
+
 }
+
