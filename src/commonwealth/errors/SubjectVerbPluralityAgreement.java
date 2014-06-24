e1326c5a002d7c847691363534d069e4a01757b9
Brian Ho <mr.brianhobo@hotmail.com>
2014-06-24 00:47:54 -0700

    added code/pseudocode for checking algorithm *NOTE* does not work so everything is commented out

--- src/commonwealth/errors/SubjectVerbPluralityAgreement.java
+++ src/commonwealth/errors/SubjectVerbPluralityAgreement.java
@@ -3,17 +3,37 @@
 
 package commonwealth.errors;
 
-import commonwealth.members.Sentence;
+import commonwealth.members.Clause;
 
 
 public class SubjectVerbPluralityAgreement{
     
+    //array of singular subjects
+    //public static final String[] singularSUBJECTS = {};
+    
+    //array of singular vernss
+    //public static final String[] singularVERBS = {};
+    
     private static String error = "subject and verb plurality do not agree";
     
-    public static String errorOf(Sentence s){
+    public static String errorOf(Clause s){
         boolean flag = false;
         
         //checking method or alogrithm goes here
+        //pretty sure that this will not work correctly
+        //im still working on it
+        //pls helperino
+        
+        //foreach(String subject : s.getSubjects()){
+        //  forreach(String verb : s.getActions()){
+        //      if(subject.ifSingularSubject() && verb.ifSingularVerb())
+        //          return error;
+        //      else if (!subject.ifSingularSubject() && !&& verb.ifSingularVerb())
+        //          return error;
+        //      else
+        //          return "correct sva";
+        //  }
+        //}
         
         if(flag)
             return error;
@@ -21,4 +41,29 @@ public class SubjectVerbPluralityAgreement{
             return "";
     }
     
+    //checks to see if the subject is part of the singular
+    //subject array
+    
+    //public static boolean ifSingularSubject(String[] array){
+    //   foreach (String sub : array){
+    //    if (this.equals(sub))
+    //        return true;
+    //    else 
+    //        return false;
+    //    }
+    //}
+    
+    //checks to see if the verb is part of the singular
+    //action array
+    
+    //public static boolean ifSingularVerb(String[] array){
+    //   foreach (String verb : array){
+    //    if (this.equals(verb))
+    //        return true;
+    //    else 
+    //        return false;
+    //    }
+    //}
+    
 }
+
